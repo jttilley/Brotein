086e8c0d38a61df93928e279e54e8122b4a9076e
Raufun <raufun05@yahoo.com>
2020-11-17 21:34:06 -0700

    added card.js

--- client/src/components/card.js
+++ client/src/components/card.js
@@ -1,1 +1,16 @@
-import React from "react";
+import React, {Component} from "react";
+import ExcerciseTable from './exercise-row';
+import MealTable from './meal-row';
+
+
+class Cards extends Component{
+    render() {
+  <div className= "card">
+    <ExcerciseTable />
+    <MealTable />
+</div>
+    }
+      
+    
+}
+export default Cards;
