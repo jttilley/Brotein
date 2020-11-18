d1d1c93ea9436974cb9d1fb2d91d9be12e65cc8e
Jason Tilley <jttilley007@yahoo.com>
2020-11-17 21:00:23 -0700

    added place holders for DB APIs

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,16 +1,34 @@
 
 module.exports = function (app) {
 
-  app.get('/api/exercise', function (req, res) {
-    console.log('Hello');
-    fetch("https://wger.de/api/v2/exercise", {
-      method: "GET"
-    })
-    .then(response => response.json())
+  // get all workouts
+  app.get('/api/new/workouts', function (req, res) {
+    console.log('Getting workouts');
   });
 
-  app.post('/api/test', function (req, res) {
-    console.log('test');
+  // get all meals
+  app.get('/api/new/meals', function (req, res) {
+    console.log('Getting meals');
+  });
+
+  // get a workout
+  app.post('/api/workout/:id', function (req, res) {
+    console.log(req.params.id);
+  });
+
+  // get a meal
+  app.post('/api/meal/:id', function (req, res) {
+    console.log(req.params.id);
+  });
+
+  // add a workout
+  app.post('/api/workout', function (req, res) {
+    console.log(req.body);
+  });
+
+  // add a meal
+  app.post('/api/meal', function (req, res) {
+    console.log(req.body);
   });
 }
 
