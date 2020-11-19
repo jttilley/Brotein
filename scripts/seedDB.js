949c9f73b66ce67e125492857d7c8182c1576211
Christal Vigil <c.vigil27@gmail.com>
2020-11-19 13:02:52 -0700

    adding seedDB, updatin models files, adding API files/start of API calls

---
+++ scripts/seedDB.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose'); 
+const Workouts = require('../models/workouts'); 
+const Meals = require('../models/meals'); 
+
+// This file empties the Posts collection and inserts the books below
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/react-auth-hooks");
+
+const workout1 = new Workouts({
+    exercise: 'Weight Lifting', 
+    sets: 20, 
+    reps: 2, 
+    weight: 50, 
+    duration: 20, 
+    body: 'Workout1'
+}); 
+console.log(workout1); 
+
+workout1.save(function (err, workout1){
+    if (err) return console.error(err); 
+}); 
+
+const meal1 = new Meals({
+    food: 'fish', 
+    protein: 20,
+    carbohydrates: 10,
+    fats: 2,
+    calories: 15, 
+    body: 'Meal1'
+}); 
+
+console.log(meal1); 
+    if (err) return console.error(err); 
+});
--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 // Dependencies 
-const express = require("express");
-const passport = require("./config/passport");
-const session = require("express-session");
+const express = require('express');
+const passport = require('./config/passport');
+const session = require('express-session');
 const MemoryStore = require('memorystore')(session);
 const logger = require('morgan');
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
+
+// const path = require("path");
+// const fs = require("fs"); 
+
 
 // Set up the express app 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Requiring our models folder for syncing 
-const db = require("./models");
+const models = require("./models");
+
+// fs.readdirSync(__dirname + '/models').forEach(function(filename){
+//   if (~filename.indexOf('.js')) require(__dirname + '/models' +  filename); 
+// })
 
 // Define middleware that handles data parsing 
 app.use(express.urlencoded({ extended: true }));
@@ -36,21 +44,28 @@ app.use(logger('dev'));
 
 
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 }
 
 // Add routes, both API and view
-require('./routes/api-routes')(app);
+require('./routes/api/api-routes')(app); 
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/react-auth-hooks', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
-});        
+});     
+
+// test that we connect successfully or if a connection error occurs 
+const db = mongoose.connection; 
+db.on('error', console.error.bind(console, 'connection error:')); 
+db.once('open', function () {
+  console.log('Successfully Connected!!'); 
+}); 
 
 // Start the server and sync Sequelize models 
-  app.listen(PORT, function() {
-    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
-  });
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});
