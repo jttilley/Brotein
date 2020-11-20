79f04b472036c8ac5e1ab3cc8e20e15912998dbb
haaleylew <66989529+haaleylew@users.noreply.github.com>
2020-11-19 20:31:08 -0700

    added all pages to route

---
+++ client/src/pages/new-workout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+function NewWorkOutPage() {
+
+    return (
+        <div>
+            <h2>New Work Out Page</h2>
+        </div>
+
+    );
+};
+export default NewWorkOutPage;
