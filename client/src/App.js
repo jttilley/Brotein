ecf693d64fbfc0df6be10186ed4741381b731cad
Jason Tilley <jttilley007@yahoo.com>
2020-11-19 21:52:02 -0700

    closer on Rafuns piece

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,9 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link, NavLink } from 'react-router-dom';
-import SignUpForm from './pages/signup';
-import SignInForm from './pages/signin';
+import AuthForm from './pages/auth';
 //import './App.css';
 //import HomePage from './pages/home';
-import HomePage from './pages/Home';
+import HomePage from './pages/home';
 import HealthPage from './pages/health';
 import NewMealPage from './pages/new-meal';
 import NewWorkOutPage from './pages/new-workout';
@@ -18,18 +17,12 @@ class App extends Component {
 
       <Router >
 
-        <Route exact path="/" component={SignUpForm} />
+        <Route exact path="/" component={AuthForm} />
         <Route exact path="/Home" component={HomePage} />
-        <Route exact path="/sign-in" component={SignInForm} />
-        <Route exact path="/health" Component={HealthPage} />
+          <Route exact path="/health" Component={HealthPage} />
         <Route exact path="/new-meal" component={NewMealPage} />
         <Route exact path="/new-workout" component={NewWorkOutPage} />
         <Route exact path="/history" component={HistoryPage} />
-
-
-
-
-
 
       </Router>
 
---
+++ client/src/pages/auth.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
+import SignUpForm from './signup';
+import SignInForm from './signin';
+
+class AuthForm extends Component {
+    
+    state = {
+      email: "",
+      password: ""
+    };
+  
+    handleChange = event => {
+      let { name, value } = event.target;
+      this.setState({ [name]: value });
+    };
+  
+    handleSubmit = event => {
+      event.preventDefault();
+      // auth
+      //   .createUserWithEmailAndPassword(this.state.email, this.state.password)
+      //   .then(user => {
+      //     // make call to the server sending in that unique id from firebase
+      //     this.setState({
+      //       toForm: true
+      //     });
+      //   })
+        // .catch(error => console.log(error));
+    };
+
+    render() {
+      return (
+      <div className="App">
+        <div className="App__Aside">Welcome to Brotein
+          <p>Brotein is an one of the favorite workout app in the world</p>
+        </div>
+        <div className="App__Form">
+          <div className="PageSwitcher">
+            <NavLink to="/sign-in" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign In</NavLink>
+            <NavLink exact to="/" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign Up</NavLink>
+          </div>
+
+          <div className="FormTitle">
+            <NavLink to="/sign-in" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign In</NavLink> or <NavLink exact to="/" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign Up</NavLink>
+          </div>
+
+          <Route exact path="/" component={SignUpForm} />
+          <Route exact path="/sign-in" component={SignInForm} />
+        </div>
+
+      </div>
+      );
+
+    
+    };
+  }
+
+  export default AuthForm;
