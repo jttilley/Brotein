572b3584a30a4fe8ab9898dce2fadfb28cde2fdb
haaleylew <66989529+haaleylew@users.noreply.github.com>
2020-11-19 22:18:17 -0700

    layouts

--- client/src/pages/history.js
+++ client/src/pages/history.js
@@ -1,12 +1,31 @@
 import React from 'react';
+import { makeStyles } from = "@material-ui/core/styles";
+import { Grid, Paper } from = "@material-ui/core";
 
+const useStyles = makeStyles((theme) => ({
+
+
+}));
 
 function HistoryPage() {
 
     return (
-        <div>
-            <h2>History Page</h2>
-        </div>
+        <Grid container spacing={3}>
+            <Grid item xs={12}>
+                <Paper className={classes.paper}>History</Paper>
+            </Grid>
+            <Grid container xs={6}
+                direction="row"
+                justify="center"
+                alignItem="stretch">
+                <Paper className={classes.paper}>YOUTUBE</Paper>
+            </Grid>
+            <Grid item xs={6}>
+                <Paper className={classes.paper}>ARTICLES</Paper>
+            </Grid>
+        </Grid>
+
+
 
     );
 };
