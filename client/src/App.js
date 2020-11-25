94cf4281fddba0fbd05058e0864b1627e9cca2e8
haaleylew <66989529+haaleylew@users.noreply.github.com>
2020-11-24 19:45:18 -0700

    blkh

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link, NavLink } from 'react-router-dom';
 import AuthForm from './pages/auth';
-import HomePage from './pages/Home';
+import HomePage from './pages/home';
 import HealthPage from './pages/health';
 import NewMealPage from './pages/new-meal';
 import NewWorkOutPage from './pages/new-workout';
@@ -14,7 +14,7 @@ class App extends Component {
       <Router >
 
         <Route exact path="/" component={AuthForm} />
-        <Route exact path="/Home" component={HomePage} />
+        <Route exact path="/home" component={HomePage} />
         <Route exact path="/health" Component={HealthPage} />
         <Route exact path="/new-meal" component={NewMealPage} />
         <Route exact path="/new-workout" component={NewWorkOutPage} />
