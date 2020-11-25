14f382105775122d4f65035a401bb5288812ec66
Raufun <raufun05@yahoo.com>
2020-11-24 21:11:30 -0700

    updated mealtable.js

--- client/src/components/ExerciseTable.js
+++ client/src/components/ExerciseTable.js
@@ -10,7 +10,7 @@ import Paper from '@material-ui/core/Paper';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Grid from '@material-ui/core/Grid';
-import "../css/exercise-row.css"
+import "../css/exerciseTable.css"
 import Box from '@material-ui/core/Box';
 
 
