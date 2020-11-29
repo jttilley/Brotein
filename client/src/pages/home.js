67ef29135d7d48ed67443e1be319e54fa38c5038
Mason McGinley <masonmcginley32@gmail.com>
2020-11-29 13:38:55 -0700

    changes

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import '../App.css';
-import Cards from '../components/cards';
+import Cards from '../components/Cards';
 import HeroSection from '../components/heroSection';
 import Navbar from '../components/navbar';
 
