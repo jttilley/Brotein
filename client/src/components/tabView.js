b23ded459d0771d124cf102b71099ddb05dbfbc0
Mason McGinley <masonmcginley32@gmail.com>
2020-12-02 21:55:37 -0700

    centered tab added

---
+++ client/src/components/tabView.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+
+
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1,
+  },
+});
+
+export default function CenteredTabs() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <Paper className={classes.root}>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        indicatorColor="primary"
+        textColor="primary"
+        centered
+      >
+        <Tab label="Fitness" value= "1" />
+        <Tab label="Meals" value="2"/>
+        <Tab label="YouTube" value= "3"/>
+        <Tab label="Recipes" value="4"/>
+
+      </Tabs>
+    </Paper>
+  );
+}
+
--- client/src/pages/history.js
+++ client/src/pages/history.js
@@ -4,14 +4,15 @@ import Navbar from '../components/navbar';
 import HistoryBanner from '../components/historyBanner';
 import ExcerciseTable from '../components/exerciseTable';
 import MealTable from '../components/mealTable';
+import CenteredTabs from '../components/tabView';
 
 function HistoryPage() {
     return (
         <>
             <Navbar />
             <HistoryBanner />
-            <ExcerciseTable />
-            <MealTable />
+            <CenteredTabs />
+            
         </>
     );
 }
