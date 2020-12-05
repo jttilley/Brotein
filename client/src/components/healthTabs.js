916eb77605a73991b366c8d26025c269b3510546
haaleylew <66989529+haaleylew@users.noreply.github.com>
2020-12-05 11:50:35 -0700

    material-ui things

--- client/src/components/healthTabs.js
+++ client/src/components/healthTabs.js
@@ -5,13 +5,19 @@ import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
 import Food from '../components/food';
 import YouTube from '../components/youtube';
+//import TabPanel from '@material-ui/lab/TabPanel';
+import TabContext from '@material-ui/lab/TabContext';
+import TabList from '@material-ui/lab/TabList';
+import { TabPanel } from '@material-ui/lab';
+import AppBar from '@material-ui/core/AppBar';
 
-const useStyles = makeStyles({
+
+const useStyles = makeStyles((theme) => ({
     root: {
         flexGrow: 1,
+        backgroundColor: theme.palette.background.paper,
     },
-});
-
+}));
 
 
 export default function CenteredTabs() {
@@ -24,18 +30,26 @@ export default function CenteredTabs() {
 
     return (
         <Paper className={classes.root}>
-            <Tabs
-                value={value}
-                onChange={handleChange}
-                indicatorColor="primary"
-                textColor="primary"
-                centered
-            >
-                <Tab label="Youtube" value="1" onChange={<YouTube />} />
-                <Tab label="Recipes" value="2" />
-                {/* <Tab label="Recipes" value="1" content={Food} /> */}
+            <TabContext value={value}>
+                <Tabs
+                    value={value}
+                    onChange={handleChange}
+                    indicatorColor="primary"
+                    textColor="primary"
+                    centered
+                >
 
-            </Tabs >
+                    <TabList onChange={handleChange} aria-label="simple tabs example">
+                        <Tab label="Youtube" value="1" />
+                        <Tab label="Recipes" value="2" />
+                    </TabList>
+                </Tabs>
+
+                <TabPanel value="1" > <span> <YouTube /> </span> </TabPanel>
+                <TabPanel value="2" ><Food /> </TabPanel>
+
+
+            </TabContext>
         </Paper >
     );
 }
