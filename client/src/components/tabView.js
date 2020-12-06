186cc4f92898803abdeec208793a1721283972dc
Mason McGinley <masonmcginley32@gmail.com>
2020-12-06 10:32:03 -0700

    history page

--- client/src/components/tabView.js
+++ client/src/components/tabView.js
@@ -3,8 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
-import Youtube from '../components/youtube';
-import Food from '../components/food';
+
 
 
 
@@ -34,8 +33,6 @@ export default function CenteredTabs() {
       >
         <Tab label="Fitness" value="1" />
         <Tab label="Meals" value="2" />
-        <Tab label="YouTube" value="3" />
-        <Tab label="Recipes" value="4" />
 
       </Tabs>
     </Paper>
