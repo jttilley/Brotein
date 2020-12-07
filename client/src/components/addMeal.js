0c25635536e73b778e07b95e372707b932168526
haaleylew <66989529+haaleylew@users.noreply.github.com>
2020-12-06 23:01:33 -0700

    editing

--- client/src/components/addMeal.js
+++ client/src/components/addMeal.js
@@ -1,6 +1,7 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import ingredients from "../utils/ingredients.json";
 import MealContext from "../utils/mealContext";
+import "../css/main.css"
 
 function AddIngredient() {
   const { meal, mealRows, handleInputChange, handleAdd } = useContext(MealContext)
@@ -9,14 +10,14 @@ function AddIngredient() {
   return (
     <div className="cards__item">
       <form>
-      <div className="workout-name">
-        <input type="text" className="form-control" name="name" placeholder="Name this Meal" onChange={handleInputChange}/> 
-        
-      </div>
+        <div className="workout-name">
+          <input type="text" className="form-control" name="name" placeholder="Name this Meal" onChange={handleInputChange} />
+
+        </div>
         <p>
           <input type="ingredient" className="form-control" list="ingredients" id="ingredient" placeholder="Add an Ingredient for your meal" name="ingredient" />
           <datalist id="ingredients">
-            { ingredients.map(ingredient => (
+            {ingredients.map(ingredient => (
               <option value={ingredient.fields.name} key={ingredient.pk} ></option>
             ))}
           </datalist>
